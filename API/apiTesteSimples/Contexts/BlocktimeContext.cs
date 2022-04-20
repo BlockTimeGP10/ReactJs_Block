30ec395c72e20fa68566a99e732048cbecd2092a
Caio Henrique Do Nascimento Soares <53976212857@info.scs>
2022-04-20 11:06:46 -0300

    sono

---
+++ API/apiTesteSimples/Contexts/BlocktimeContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using apiTesteSimples.Domains;
+
+#nullable disable
+
+namespace apiTesteSimples.Contexts
+{
+    public partial class BlocktimeContext : DbContext
+    {
+        public BlocktimeContext()
+        {
+        }
+
+        public BlocktimeContext(DbContextOptions<BlocktimeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Empresa> Empresas { get; set; }
+        public virtual DbSet<Equipamento> Equipamentos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=NOTE0113D2\\SQLEXPRESS; initial catalog=BLOCKTIME_TRACKING; user Id=sa; pwd=Senai@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Empresa>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpresa)
+                    .HasName("PK__EMPRESA__75D2CED4A3D3E588");
+
+                entity.ToTable("EMPRESA");
+
+                entity.Property(e => e.IdEmpresa)
+                    .ValueGeneratedNever()
+                    .HasColumnName("idEmpresa");
+
+                entity.Property(e => e.NomeEmpresa)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("nomeEmpresa");
+            });
+
+            modelBuilder.Entity<Equipamento>(entity =>
+            {
+                entity.HasKey(e => e.IdEquipamentos)
+                    .HasName("PK__EQUIPAME__05C45F544EEB0135");
+
+                entity.ToTable("EQUIPAMENTOS");
+
+                entity.Property(e => e.IdEquipamentos).HasColumnName("idEquipamentos");
+
+                entity.Property(e => e.IdEmpresa).HasColumnName("idEmpresa");
+
+                entity.Property(e => e.Lat)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("lat");
+
+                entity.Property(e => e.Lng)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("lng");
+
+                entity.Property(e => e.Mac)
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .HasColumnName("mac");
+
+                entity.HasOne(d => d.IdEmpresaNavigation)
+                    .WithMany(p => p.Equipamentos)
+                    .HasForeignKey(d => d.IdEmpresa)
+                    .HasConstraintName("FK__EQUIPAMEN__idEmp__38996AB5");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ API/apiTesteSimples/Controllers/EquipamentoesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using apiTesteSimples.Contexts;
+using apiTesteSimples.Domains;
+
+namespace apiTesteSimples.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipamentoesController : ControllerBase
+    {
+        private readonly BlocktimeContext _context;
+
+        public EquipamentoesController(BlocktimeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Equipamentoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Equipamento>>> GetEquipamentos()
+        {
+            return await _context.Equipamentos.ToListAsync();
+        }
+
+        // GET: api/Equipamentoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Equipamento>> GetEquipamento(int id)
+        {
+            var equipamento = await _context.Equipamentos.FindAsync(id);
+
+            if (equipamento == null)
+            {
+                return NotFound();
+            }
+
+            return equipamento;
+        }
+
+        // PUT: api/Equipamentoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEquipamento(int id, Equipamento equipamento)
+        {
+            if (id != equipamento.IdEquipamentos)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(equipamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Equipamentoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Equipamento>> PostEquipamento(Equipamento equipamento)
+        {
+            _context.Equipamentos.Add(equipamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEquipamento", new { id = equipamento.IdEquipamentos }, equipamento);
+        }
+
+        // DELETE: api/Equipamentoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEquipamento(int id)
+        {
+            var equipamento = await _context.Equipamentos.FindAsync(id);
+            if (equipamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Equipamentos.Remove(equipamento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EquipamentoExists(int id)
+        {
+            return _context.Equipamentos.Any(e => e.IdEquipamentos == id);
+        }
+    }
+}
